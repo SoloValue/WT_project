97ab9d6f2b08ac66338677a4bccd8e9e8f9762df
Matteo <matteomoscatelli98@gmail.com>
2023-11-02 20:16:03 +0200

    Finilazied base version of backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from shop import views
+
+router = routers.DefaultRouter()
+router.register('categories', views.CategoryView, 'category')
+router.register('products', views.ProductView, 'product')
+router.register('users', views.UserView, 'user')
+router.register('orders', views.OrderView, 'order')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
 
 # Create your views here.
+# ModelViewSet class provide the implementation for CRUD operations
+class CategoryView(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+class ProductView(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+class UserView(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class OrderView(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
